b66da7d54cbea7459c782456485d0f0d48b3012f
Pedro Passos <pedro.passos081@gmail.com>
2024-03-11 14:43:59 -0300

    Adicionando o componente Botao e o grid

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from "react-grid-system";
 import Cabecalho from "./components/Cabecalho";
 import CampoTexto from "./components/CampoTexto";
 import { Card } from "./components/Card";
@@ -11,17 +12,24 @@ function App() {
   return (
     <ProvedorTema>
       <Estilos />
-      <Cabecalho />
-      <Card>
-        <Tipografia variante='h1' componente='h1'>
-          freelando
-        </Tipografia>
-        <Tipografia variante='body' component='body'>
-          Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
-        </Tipografia>
-        <CampoTexto titulo='Nome Completo' />
-      </Card>
-      <Rodape />
+      <Container>
+        <Row justify="center">
+          <Col lg={6}>
+            <Card>
+              <Tipografia variante='h1' componente='h1'>
+                freelando
+              </Tipografia>
+              <Tipografia variante='body' component='body'>
+                Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
+              </Tipografia>
+              <CampoTexto titulo='Nome Completo' />
+            </Card>
+          </Col>
+        </Row>
+        <Cabecalho />
+
+        <Rodape />
+      </Container>
     </ProvedorTema >
   );
 }
