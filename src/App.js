737874fd637f07e25dda9bbf97bab6be836778af
PedroPassos081 <pedro.passos081@gmail.com>
2024-03-13 17:12:29 -0300

    Erro de sintaxe/digitação

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
               <Tipografia variante='h1' componente='h1'>
                 freelando
               </Tipografia>
-              <Tipografia variante='body' component='body'>
+              <Tipografia variante='body' componente='body'>
                 Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
               </Tipografia>
               <CampoTexto titulo='Nome Completo' />
