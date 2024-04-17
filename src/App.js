a5d9aaa00877e1f053505f2e27b5422e17df00c8
PedroPassos081 <pedro.passos081@gmail.com>
2024-04-17 10:13:25 -0300

    Adicionando todos os campos do projeto

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Estilos } from "./components/EstilosGlobais/Estilos";
 import ProvedorTema from "./components/ProvedorTema";
 import Rodape from "./components/Rodape";
 import Tipografia from "./components/Tipografia";
+import Botao from "./components/Botao";
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
       <Cabecalho />
       <Container>
         <Row justify="center">
-          <Col lg={6}>
+          <Col lg={6} md={8} sm={12}>
             <Card >
               <Tipografia variante='h1' componente='h1'>
                 freelando
@@ -23,7 +24,46 @@ function App() {
               <Tipografia variante='body' componente='body'>
                 Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
               </Tipografia>
-              <CampoTexto titulo='Nome Completo' />
+              <Row>
+                <Col>
+                  <CampoTexto titulo='Nome Completo' />
+                </Col>
+              </Row>
+              <Row>
+                <Col lg={4} md={4} sm={4} >
+                  <CampoTexto titulo='Estado' />
+                </Col>
+                <Col lg={8} md={8} sm={8} >
+                  <CampoTexto titulo='Cidade' />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <CampoTexto titulo='E-mail' />
+                </Col>
+              </Row>
+              <Row>
+                <Col lg={6} md={6} sm={6} >
+                  <CampoTexto titulo='Senha' />
+                </Col>
+                <Col lg={6} md={6} sm={6} >
+                  <CampoTexto titulo='Repita a senha' />
+                </Col>
+              </Row>
+              <Row>
+                <Col lg={6} md={6} sm={6}>
+                  <Botao variante="secundaria">
+                    Anterior
+                  </Botao>
+                </Col>
+                <Col lg={6} md={6} sm={6}>
+                  <div style={{ textAlign: 'right' }}>
+                    <Botao >
+                      Próxima
+                    </Botao>
+                  </div>
+                </Col>
+              </Row>
             </Card>
           </Col>
         </Row>
